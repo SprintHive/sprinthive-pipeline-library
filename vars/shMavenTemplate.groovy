55480d148dbc00433514808075bde2a2565d1b25
Dale Tristram <dale.tristram@sprinthive.com>
2017-08-03 09:39:14 +0200

    Remove mounting of mvn config secret

--- vars/shMavenTemplate.groovy
+++ vars/shMavenTemplate.groovy
@@ -14,8 +14,7 @@ def call(Map parameters = [:], body) {
                     [name: 'maven', image: "${mavenImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true,
                      envVars: [
                              [key: 'MAVEN_OPTS', value: '-Duser.home=/root/']]]],
-            volumes: [secretVolume(secretName: 'jenkins-maven-settings', mountPath: '/root/.m2'),
-                      secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker'),
+            volumes: [ secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker'),
                       secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg'),
                       secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
                       secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh'),
