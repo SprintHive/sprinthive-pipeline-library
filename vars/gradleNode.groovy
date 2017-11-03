7e5f72da36121a9de5b6f703ea85e2bf8a17b694
Jon Lai Lam <jon@lailam.co.za>
2017-11-03 18:45:06 +0200

    Changed the gradleNode to use the gradleTemplate

--- vars/gradleNode.groovy
+++ vars/gradleNode.groovy
@@ -5,7 +5,7 @@ def call(Map parameters = [:], body) {
     def defaultLabel = "gradle.${env.JOB_NAME}.${env.BUILD_NUMBER}".replace('-', '_').replace('/', '_')
     def label = parameters.get('label', defaultLabel)
 
-    mavenTemplate(parameters) {
+    gradleTemplate(parameters) {
         node(label) {
             body()
         }
