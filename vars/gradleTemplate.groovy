8c8cb4fad0216db83c903a706e28c4db478408a7
Jon Lai Lam <jon@lailam.co.za>
2017-11-03 18:36:14 +0200

    Added a gradleNode

---
+++ vars/gradleTemplate.groovy
@@ -0,0 +1,23 @@
+#!/usr/bin/groovy
+
+def call(Map parameters = [:], body) {
+    def defaultLabel = buildId('maven')
+    def label = parameters.get('label', defaultLabel)
+
+    def gradleImage = parameters.get('gradleImage', 'gradle:4.2-jdk-8-alpine')
+    def dockerImage = parameters.get('dockerImage', 'docker:stable')
+    def inheritFrom = parameters.get('inheritFrom', 'base')
+
+    echo "Deploying Gradle and Docker image"
+
+    podTemplate(label: label, inheritFrom: "${inheritFrom}",
+            containers: [
+                [name: 'gradle', image: "${gradleImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true],
+                [name: 'docker', image: "${dockerImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true]],
+            volumes: [configMapVolume(configMapName: 'jenkins-maven-settings', mountPath: '/root/.m2'),
+                      hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
+            envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock'], [key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/']]
+    ) {
+        body()
+    }
+}
