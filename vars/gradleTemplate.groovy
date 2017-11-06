3032e26ebd8caadedb4f7fab7f1c984573a73aca
Jon Lai Lam <jon@lailam.co.za>
2017-11-06 13:47:01 +0200

    Changing the gradleImage

--- vars/gradleTemplate.groovy
+++ vars/gradleTemplate.groovy
@@ -4,7 +4,7 @@ def call(Map parameters = [:], body) {
     def defaultLabel = buildId('gradle')
     def label = parameters.get('label', defaultLabel)
 
-    def gradleImage = parameters.get('gradleImage', 'gradle:4.2-jdk8-alpine')
+    def gradleImage = parameters.get('gradleImage', 'gradle:4.2-jdk8')
     def dockerImage = parameters.get('dockerImage', 'docker:stable')
     def inheritFrom = parameters.get('inheritFrom', 'base')
 
