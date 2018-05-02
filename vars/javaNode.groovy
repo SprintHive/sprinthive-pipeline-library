39c3805cfb8cb897b4bf60a7edfc103952c0bcf6
Dale Tristram <dale.tristram@sprinthive.com>
2018-05-02 22:27:46 +0200

    Fix javaNode

--- vars/javaNode.groovy
+++ vars/javaNode.groovy
@@ -5,7 +5,7 @@ def call(Map parameters = [:], body) {
     def defaultLabel = "gradle.${env.JOB_NAME}.${env.BUILD_NUMBER}".replace('-', '_').replace('/', '_')
     def label = parameters.get('label', defaultLabel)
 
-    gradleTemplate(parameters) {
+    javaTemplate(parameters) {
         node(label) {
             body()
         }
