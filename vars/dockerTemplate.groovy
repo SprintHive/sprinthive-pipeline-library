bca6f91ccda1e9afc191265860dbe144afd55d0e
Dale Tristram <dale.tristram@sprinthive.com>
2018-05-02 22:13:51 +0200

    Add helm to dockerTemplate

--- vars/dockerTemplate.groovy
+++ vars/dockerTemplate.groovy
@@ -5,12 +5,14 @@ def call(Map parameters = [:], body) {
     def label = parameters.get('label', defaultLabel)
 
     def dockerImage = parameters.get('dockerImage', 'docker:stable')
+    def helmImage = parameters.get('helmImage', 'lachlanevenson/k8s-helm:v2.8.2')
     def inheritFrom = parameters.get('inheritFrom', 'base')
 
     echo "Starting pod with docker"
 
-	podTemplate(label: label, inheritFrom: "${inheritFrom}",
+	podTemplate(label: label, inheritFrom: "${inheritFrom}", serviceAccount: "helm",
 			containers: [
+					containerTemplate(name: 'helm', image: "${helmImage}", command: 'cat', ttyEnabled: true),
 					containerTemplate(name: 'docker', image: "${dockerImage}", command: 'cat', ttyEnabled: true, privileged: true)],
 			volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
 			envVars: [
