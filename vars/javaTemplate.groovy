8ef6305cd9e911437697c1658e3a0fa6e1a14bd0
Dale Tristram <dale.tristram@sprinthive.com>
2018-05-02 22:10:48 +0200

    Add javaTemplate

---
+++ vars/javaTemplate.groovy
@@ -0,0 +1,26 @@
+#!/usr/bin/groovy
+
+def call(Map parameters = [:], body) {
+    def defaultLabel = buildId('gradle')
+    def label = parameters.get('label', defaultLabel)
+
+    def gradleImage = parameters.get('gradleImage', 'bitstack701/base-gradle:v3.0.2')
+    def dockerImage = parameters.get('dockerImage', 'docker:stable')
+    def helmImage = parameters.get('helmImage', 'lachlanevenson/k8s-helm:v2.8.2')
+    def inheritFrom = parameters.get('inheritFrom', 'base')
+
+    echo "Starting pod with gradle and docker"
+
+    podTemplate(label: label, inheritFrom: "${inheritFrom}", serviceAccount: "helm",
+            containers: [
+				containerTemplate(name: 'helm', image: "${helmImage}", command: 'cat', ttyEnabled: true),
+                containerTemplate(name: 'gradle', image: "${gradleImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true),
+                containerTemplate(name: 'docker', image: "${dockerImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true)],
+            volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
+            envVars: [
+                envVar(key: 'DOCKER_HOST', value: 'unix:///var/run/docker.sock'),
+                envVar(key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/')]
+    ) {
+        body()
+    }
+}
