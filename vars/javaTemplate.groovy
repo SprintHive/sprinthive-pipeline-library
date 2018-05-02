153c18a71b4a2fc0051a734ed87ef5bc22e69207
Dale Tristram <dale.tristram@sprinthive.com>
2018-05-02 22:12:29 +0200

    Add maven container to java template

--- vars/javaTemplate.groovy
+++ vars/javaTemplate.groovy
@@ -5,6 +5,7 @@ def call(Map parameters = [:], body) {
     def label = parameters.get('label', defaultLabel)
 
     def gradleImage = parameters.get('gradleImage', 'bitstack701/base-gradle:v3.0.2')
+    def mavenImage = parameters.get('mavenImage', 'maven:3.5-jdk-8-alpine')
     def dockerImage = parameters.get('dockerImage', 'docker:stable')
     def helmImage = parameters.get('helmImage', 'lachlanevenson/k8s-helm:v2.8.2')
     def inheritFrom = parameters.get('inheritFrom', 'base')
@@ -15,8 +16,10 @@ def call(Map parameters = [:], body) {
             containers: [
 				containerTemplate(name: 'helm', image: "${helmImage}", command: 'cat', ttyEnabled: true),
                 containerTemplate(name: 'gradle', image: "${gradleImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true),
+                containerTemplate(name: 'maven', image: "${mavenImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true, envVars: [ envVar(key: 'MAVEN_OPTS', value: '-Duser.home=/root/') ]),
                 containerTemplate(name: 'docker', image: "${dockerImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true)],
-            volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
+            volumes: [configMapVolume(configMapName: 'jenkins-maven-settings', mountPath: '/root/.m2'),
+                      hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
             envVars: [
                 envVar(key: 'DOCKER_HOST', value: 'unix:///var/run/docker.sock'),
                 envVar(key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/')]
