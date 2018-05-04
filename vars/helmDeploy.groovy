784c01d182436ef09503e7313c69a0691e74b0c5
Dale Tristram <dale.tristram@sprinthive.com>
2018-05-04 18:33:34 +0200

    Fix imageTag reference

--- vars/helmDeploy.groovy
+++ vars/helmDeploy.groovy
@@ -10,6 +10,6 @@ def call(body) {
     container('helm') {
         sh "helm init --client-only"
         sh "helm repo add sprinthive-service-dev-charts https://sprinthive-service-dev-charts.storage.googleapis.com/"
-        sh "helm upgrade ${config.releaseName} --namespace ${config.namespace} -i --reset-values --recreate-pods --wait sprinthive-service-dev-charts/${config.chartName} --set image.tag=${imageTag}"
+        sh "helm upgrade ${config.releaseName} --namespace ${config.namespace} -i --reset-values --recreate-pods --wait sprinthive-service-dev-charts/${config.chartName} --set image.tag=${config.imageTag}"
     }
 }
