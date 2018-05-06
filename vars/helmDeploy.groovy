834be7cec883cafe0ed22139088b0f1aa7ef3a39
Dale Tristram <dale.tristram@sprinthive.com>
2018-05-06 22:12:10 +0200

    Allow setting overrides in helmDeploy

--- vars/helmDeploy.groovy
+++ vars/helmDeploy.groovy
@@ -7,9 +7,14 @@ def call(body) {
     body.delegate = config
     body()
 
+    def overrides = ""
+    for (String override : config.overrides) {
+        overrides += "--set " + override;
+    }
+
     container('helm') {
         sh "helm init --client-only"
         sh "helm repo add sprinthive-service-dev-charts https://sprinthive-service-dev-charts.storage.googleapis.com/"
-        sh "helm upgrade ${config.releaseName} --namespace ${config.namespace} -i --reset-values --recreate-pods --wait sprinthive-service-dev-charts/${config.chartName} --set image.tag=${config.imageTag}"
+        sh "helm upgrade ${config.releaseName} --namespace ${config.namespace} -i --reset-values --recreate-pods --wait sprinthive-service-dev-charts/${config.chartName} --set image.tag=${config.imageTag} ${overrides}"
     }
 }
