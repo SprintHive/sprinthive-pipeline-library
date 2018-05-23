e02a334b38d62d61a7a5d89941dcae9e61b65c55
Dale Tristram <dale.tristram@sprinthive.com>
2018-05-23 14:27:33 +0200

    Make helm tiller namespace specific

--- vars/helmDeploy.groovy
+++ vars/helmDeploy.groovy
@@ -9,6 +9,6 @@ def call(config) {
     container('helm') {
         sh "helm init --client-only"
         sh "helm repo add sprinthive-service-dev-charts https://sprinthive-service-dev-charts.storage.googleapis.com/"
-        sh "helm upgrade ${config.releaseName} --namespace ${config.namespace} -i --reset-values --wait sprinthive-service-dev-charts/${config.chartName} --set image.tag=${config.imageTag} ${overrides}"
+        sh "helm --tiller-namespace ${config.namespace} upgrade ${config.releaseName} --namespace ${config.namespace} -i --reset-values --wait sprinthive-service-dev-charts/${config.chartName} --set image.tag=${config.imageTag} ${overrides}"
     }
 }
