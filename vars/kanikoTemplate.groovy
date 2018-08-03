71d0c8fa040d943fb4d4dfcf2431c3a36bb1f4be
Dale Tristram <dale.tristram@sprinthive.com>
2018-08-03 21:02:37 +0200

    Use latest kaniko image to try get around bug

--- vars/kanikoTemplate.groovy
+++ vars/kanikoTemplate.groovy
@@ -4,7 +4,7 @@ def call(Map parameters = [:], body) {
     def defaultLabel = buildId('kaniko')
     def label = parameters.get('label', defaultLabel)
 
-    def kanikoImage = parameters.get('kanikoImage', 'gcr.io/kaniko-project/executor:debug')
+    def kanikoImage = parameters.get('kanikoImage', 'gcr.io/kaniko-project/executor:debug-53b5fb41d2e08dc45a491836a8e8b0fff3961f0e')
     def jnlpImage = parameters.get('jnlpImage', 'jenkins/jnlp-slave:alpine')
     def helmImage = parameters.get('helmImage', 'lachlanevenson/k8s-helm:v2.9.1')
     def inheritFrom = parameters.get('inheritFrom', 'base')
