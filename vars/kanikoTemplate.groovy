f40c39cc37f690ff3fbbd54f16bde831b9f378f5
Dale Tristram <dale.tristram@sprinthive.com>
2018-08-03 23:00:45 +0200

    Use self-built kaniko image that lacks segfault bug

--- vars/kanikoTemplate.groovy
+++ vars/kanikoTemplate.groovy
@@ -4,7 +4,7 @@ def call(Map parameters = [:], body) {
     def defaultLabel = buildId('kaniko')
     def label = parameters.get('label', defaultLabel)
 
-    def kanikoImage = parameters.get('kanikoImage', 'gcr.io/kaniko-project/executor:debug-71c83e369cb6e3e5637a341dee99118407a760fe')
+    def kanikoImage = parameters.get('kanikoImage', 'quay.io/sprinthive/kaniko:db08ecb4336f-debug')
     def jnlpImage = parameters.get('jnlpImage', 'jenkins/jnlp-slave:alpine')
     def helmImage = parameters.get('helmImage', 'lachlanevenson/k8s-helm:v2.9.1')
     def inheritFrom = parameters.get('inheritFrom', 'base')
