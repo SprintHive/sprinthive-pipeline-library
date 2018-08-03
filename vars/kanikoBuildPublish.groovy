44beee12c31638920ab031eb24662bdcf547664e
Dale Tristram <dale.tristram@sprinthive.com>
2018-08-03 14:29:35 +0200

    Allow Dockerfile override

--- vars/kanikoBuildPublish.groovy
+++ vars/kanikoBuildPublish.groovy
@@ -3,9 +3,10 @@
 def call(config) {
     container(name: 'kaniko', shell: '/busybox/sh') {
         withCredentials([file(credentialsId: config.registryCredentialsId, variable: 'GOOGLE_APPLICATION_CREDENTIALS')]) {
+            def dockerFile = config.dockerFileOverride != null ? config.dockerFileOverride : "Dockerfile"
             for (String tag: config.tags) {
                 sh """#!/busybox/sh
-                /kaniko/executor -f `pwd`/Dockerfile -c `pwd` --destination=${config.registryUrl}/${config.registryBasePath}:${config.imageName}:${tag}
+                /kaniko/executor -f `pwd`/${dockerFile} -c `pwd` --destination=${config.registryUrl}/${config.registryBasePath}:${config.imageName}:${tag}
                 """
             }
         }
