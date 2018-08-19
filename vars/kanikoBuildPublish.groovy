7345cb544fc9558f6d0bbb64b2a2d6e46be041f1
Dale Tristram <dale.tristram@sprinthive.com>
2018-08-19 20:38:58 +0200

    Add creds for skaffold

--- vars/kanikoBuildPublish.groovy
+++ vars/kanikoBuildPublish.groovy
@@ -2,9 +2,11 @@
 
 def call(config) {
     container(name: 'skaffold') {
-        def dockerFile = config.dockerFileOverride != null ? config.dockerFileOverride : "Dockerfile"
-        for (String tag: config.tags) {
-            sh "skaffold run"
+        withCredentials([file(credentialsId: config.registryCredentialsId, variable: 'GOOGLE_APPLICATION_CREDENTIALS')]) {
+            def dockerFile = config.dockerFileOverride != null ? config.dockerFileOverride : "Dockerfile"
+            for (String tag: config.tags) {
+                sh "skaffold run"
+            }
         }
     }
 }
