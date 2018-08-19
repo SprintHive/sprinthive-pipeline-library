436d0d029d368bdcac65d0ace4a1ead055da5bf4
Dale Tristram <dale.tristram@sprinthive.com>
2018-08-19 19:56:47 +0200

    Attempt #2 of sh command fix

--- vars/kanikoBuildPublish.groovy
+++ vars/kanikoBuildPublish.groovy
@@ -2,12 +2,12 @@
 
 def call(config) {
     container(name: 'kaniko', shell: '/busybox/sh') {
-        withEnv(['PATH+EXTRA=/busybox']) {
-            withCredentials([file(credentialsId: config.registryCredentialsId, variable: 'GOOGLE_APPLICATION_CREDENTIALS')]) {
-                def dockerFile = config.dockerFileOverride != null ? config.dockerFileOverride : "Dockerfile"
-                for (String tag: config.tags) {
-                    sh "/kaniko/executor -f `pwd`/${dockerFile} -c `pwd` --destination=${config.registryHost}/${config.imagePath}:${tag}"
-                }
+        withCredentials([file(credentialsId: config.registryCredentialsId, variable: 'GOOGLE_APPLICATION_CREDENTIALS')]) {
+            def dockerFile = config.dockerFileOverride != null ? config.dockerFileOverride : "Dockerfile"
+            for (String tag: config.tags) {
+                sh """#!/busybox/sh
+                /kaniko/executor -f `pwd`/${dockerFile} -c `pwd` --destination=${config.registryHost}/${config.imagePath}:${tag}
+                """
             }
         }
     }
