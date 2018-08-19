c2d40d6b2faa166d1fa45d4c33990121478a8266
Dale Tristram <dale.tristram@sprinthive.com>
2018-08-19 16:58:02 +0200

    Use busybox/sh for kaniko

--- vars/kanikoBuildPublish.groovy
+++ vars/kanikoBuildPublish.groovy
@@ -5,7 +5,9 @@ def call(config) {
         withCredentials([file(credentialsId: config.registryCredentialsId, variable: 'GOOGLE_APPLICATION_CREDENTIALS')]) {
             def dockerFile = config.dockerFileOverride != null ? config.dockerFileOverride : "Dockerfile"
             for (String tag: config.tags) {
-                sh "/kaniko/executor -f `pwd`/${dockerFile} -c `pwd` --destination=${config.registryHost}/${config.imagePath}:${tag}"
+                sh '''#!/busybox/sh
+                /kaniko/executor -f `pwd`/${dockerFile} -c `pwd` --destination=${config.registryHost}/${config.imagePath}:${tag}
+                '''
             }
         }
     }
