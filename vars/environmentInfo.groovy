d955abc92b51da9b71f1e8e3d68cede158742ebe
Marco Theart <marco.theart@sprinthive.com>
2018-10-22 10:38:58 +0200

    Setting deployStage based on SERVICE_CHART env variable with fallback to default behaviour

--- vars/environmentInfo.groovy
+++ vars/environmentInfo.groovy
@@ -6,36 +6,31 @@ def call(scmInfo) {
         error('Git branch is null..')
     }
 
-    def deployStage = null
+    def deployStage = env.SERVICE_CHART
+    def rawBranch = scmInfo.GIT_BRANCH.substring(scmInfo.GIT_BRANCH.lastIndexOf('/')+1)
+    def branchParts = rawBranch.split('-')
     def inferredBranch = null
     def multivariateTest = null
-    if (scmInfo.GIT_LOCAL_BRANCH) {
-        inferredBranch = scmInfo.GIT_LOCAL_BRANCH
-        echo "Using local branch: ${inferredBranch}"
+    // A multivariate test has a branch with naming convention <originbranch-mvt-mvtname>. E.g. dev-mvt-lowagereq
+    if (branchParts.length > 2 && branchParts[1] == "mvt") {
+        inferredBranch = branchParts[0]
+        multivariateTest = branchParts[2]
     } else {
+        inferredBranch = rawBranch
+    }
 
-        // TODO: incorporate multivariate tests in local branch config?
-
-        def rawBranch = scmInfo.GIT_BRANCH.substring(scmInfo.GIT_BRANCH.lastIndexOf('/')+1)
-        def branchParts = rawBranch.split('-')
-        // A multivariate test has a branch with naming convention <originbranch-mvt-mvtname>. E.g. dev-mvt-lowagereq
-        if (branchParts.length > 2 && branchParts[1] == "mvt") {
-            inferredBranch = branchParts[0]
-            multivariateTest = branchParts[2]
+    if (!deployStage) {
+        if (inferredBranch.equals("dev")) {
+            deployStage = "dev"
+        } else if (inferredBranch.equals('master')) {
+            deployStage = 'pre-prod'
         } else {
-            inferredBranch = rawBranch
+            deployStage = inferredBranch
         }
-    }
-
-    if (inferredBranch.equals("dev")) {
-        deployStage = "dev"
-    } else if (inferredBranch.equals('master')) {
-        deployStage = 'pre-prod'
+        echo "SERVICE_CHART env variable not set, deployStage set based on branch: ${deployStage}"
     } else {
-        deployStage = inferredBranch
+        echo "deployStage set to SERVICE_CHART env variable: ${deployStage}"
     }
-
-
 
     return [
         "branch": inferredBranch,
