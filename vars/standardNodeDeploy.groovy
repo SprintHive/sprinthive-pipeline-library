6d541b1377e0e220627baeb550b75b47b404acde
Dale Tristram <dale.tristram@sprinthive.com>
2018-11-08 08:51:23 +0200

    Add integration test stage

--- vars/standardNodeDeploy.groovy
+++ vars/standardNodeDeploy.groovy
@@ -3,10 +3,12 @@
 def call(config) {
     def versionTag = ''
     def dockerImage
+    def targetNamespace
 
     nodejsNode {
         def scmInfo = checkout scm
         def envInfo = environmentInfo(scmInfo)
+        targetNamespace = envInfo.deployStage
         echo "Current branch is: ${envInfo.branch}"
         echo "Deploy namespace is: ${envInfo.deployStage}"
 
@@ -44,11 +46,31 @@ def call(config) {
                 chartRepoOverride: config.chartRepoOverride
             ])
         }
+    }
 
-        if (env.POST_BUILD_TRIGGER_JOB != null) {
-            stage("Trigger ${env.POST_BUILD_TRIGGER_JOB}") {
-                build job: env.POST_BUILD_TRIGGER_JOB, parameters: [string(name: 'IMAGE_TAG', value: versionTag)], wait: false
-            }
+    if (config.runIntegrationTest != null && config.runIntegrationTest) {
+        stage("Integration test") {
+            def label = "node-test-${UUID.randomUUID().toString()}"
+            podTemplate(label: label, containers: [
+                containerTemplate(name: 'node', image: 'mhart/alpine-node:8', ttyEnabled: true, command: 'cat'),
+              ], namespace: targetNamespace, envVars: [
+                envVar(key: 'JENKINS_URL', value: 'http://cicd-jenkins.infra:8080/'),
+                envVar(key: 'JENKINS_TUNNEL', value: 'cicd-jenkins-agent.infra:50000')
+              ]) {
+                node(label) {
+                    checkout scm
+                    container('node') {
+                        sh 'yarn'
+                        sh 'yarn integration-test'
+                    }
+                }
+              }
+        }
+    }
+
+    if (env.POST_BUILD_TRIGGER_JOB != null) {
+        stage("Trigger ${env.POST_BUILD_TRIGGER_JOB}") {
+            build job: env.POST_BUILD_TRIGGER_JOB, parameters: [string(name: 'IMAGE_TAG', value: versionTag)], wait: false
         }
     }
 }
