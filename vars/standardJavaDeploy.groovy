ead9765eb6b7f0d890a10d88aa5b9612f8910aaa
Dale Tristram <dale.tristram@sprinthive.com>
2018-11-08 15:53:29 +0200

    Add integration test option to java service

--- vars/standardJavaDeploy.groovy
+++ vars/standardJavaDeploy.groovy
@@ -48,11 +48,31 @@ def call(config) {
                 chartRepoOverride: config.chartRepoOverride
             ])
         }
+    }
 
-        if (env.POST_BUILD_TRIGGER_JOB != null) {
-            stage("Trigger ${env.POST_BUILD_TRIGGER_JOB}") {
-                build job: env.POST_BUILD_TRIGGER_JOB, parameters: [string(name: 'IMAGE_TAG', value: versionTag)], wait: false
-            }
+    def gradleImage = parameters.get('gradleImage', 'bitstack701/base-gradle:v3.0.2')
+    if (config.runIntegrationTest != null && config.runIntegrationTest) {
+        stage("Integration test") {
+            def label = "java-test-${UUID.randomUUID().toString()}"
+            podTemplate(label: label, containers: [
+                containerTemplate(name: 'gradle', image: 'bitstack701/base-gradle:v3.0.2', ttyEnabled: true, command: 'cat'),
+              ], namespace: targetNamespace, envVars: [
+                envVar(key: 'JENKINS_URL', value: 'http://cicd-jenkins.infra:8080/'),
+                envVar(key: 'JENKINS_TUNNEL', value: 'cicd-jenkins-agent.infra:50000')
+              ]) {
+                node(label) {
+                    checkout scm
+                    container('node') {
+                        sh 'gradle -i integrationTest'
+                    }
+                }
+              }
+        }
+    }
+
+    if (env.POST_BUILD_TRIGGER_JOB != null) {
+        stage("Trigger ${env.POST_BUILD_TRIGGER_JOB}") {
+            build job: env.POST_BUILD_TRIGGER_JOB, parameters: [string(name: 'IMAGE_TAG', value: versionTag)], wait: false
         }
     }
 }
