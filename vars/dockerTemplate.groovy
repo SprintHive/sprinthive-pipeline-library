17504cdee9c8e8bbfffded5f51cfc34747ac94c5
Dale Tristram <dale.tristram@sprinthive.com>
2018-11-12 09:17:56 +0200

    Add clair container scan to pipeline

--- vars/dockerTemplate.groovy
+++ vars/dockerTemplate.groovy
@@ -5,20 +5,59 @@ def call(Map parameters = [:], body) {
     def label = parameters.get('label', defaultLabel)
 
     def dockerImage = parameters.get('dockerImage', 'docker:stable')
+    def clairScannerImage = parameters.get('clairScannerImage', 'objectiflibre/clair-scanner:latest')
     def helmImage = parameters.get('helmImage', 'lachlanevenson/k8s-helm:v2.9.1')
     def inheritFrom = parameters.get('inheritFrom', 'base')
 
-    echo "Starting pod with docker"
+    echo "Starting docker jenkins worker"
 
-	podTemplate(label: label, inheritFrom: "${inheritFrom}", serviceAccount: "helm",
-			containers: [
-					containerTemplate(name: 'helm', image: "${helmImage}", command: 'cat', ttyEnabled: true),
-					containerTemplate(name: 'docker', image: "${dockerImage}", command: 'cat', ttyEnabled: true, privileged: true)],
-			volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
-			envVars: [
-                envVar(key: 'DOCKER_HOST', value: 'unix:///var/run/docker.sock'),
-                envVar(key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/')]
+    podTemplate(label: label, inheritFrom: "${inheritFrom}", yaml: """
+    apiVersion: v1
+    kind: Pod
+    spec:
+      serviceAccount: helm
+      containers:
+      - name: docker
+        image: ${dockerImage}
+        command:
+        - cat
+        tty: true
+        securityContext:
+          privileged: true
+        env:
+        - name: DOCKER_HOST
+          value: unix:///var/run/docker.sock
+        volumeMounts:
+        - name: docker-socket
+          mountPath: /var/run/docker.sock
+      - name: clairscanner
+        image: ${clairScannerImage}
+        command:
+        - cat
+        tty: true
+        securityContext:
+          privileged: true
+        env:
+        - name: DOCKER_HOST
+          value: unix:///var/run/docker.sock
+        - name: POD_IP
+          valueFrom:
+            fieldRef:
+              fieldPath: status.podIP
+        volumeMounts:
+        - name: docker-socket
+          mountPath: /var/run/docker.sock
+        ports:
+        - containerPort: 9279
+          name: http
+          protocol: TCP
+      - name: helm
+        image: ${helmImage}
+        command:
+        - cat
+        tty: true
+    """
     ) {
-		body()
-	}
+    body()
+  }
 }
--- vars/javaTemplate.groovy
+++ vars/javaTemplate.groovy
@@ -5,25 +5,79 @@ def call(Map parameters = [:], body) {
     def label = parameters.get('label', defaultLabel)
 
     def gradleImage = parameters.get('gradleImage', 'bitstack701/base-gradle:v3.0.2')
-    def mavenImage = parameters.get('mavenImage', 'maven:3.5-jdk-8-alpine')
+    def clairScannerImage = parameters.get('clairScannerImage', 'objectiflibre/clair-scanner:latest')
     def dockerImage = parameters.get('dockerImage', 'docker:stable')
     def helmImage = parameters.get('helmImage', 'lachlanevenson/k8s-helm:v2.9.1')
     def inheritFrom = parameters.get('inheritFrom', 'base')
 
-    echo "Starting pod with gradle and docker"
+    echo "Starting java-centric jenkins worker"
 
-    podTemplate(label: label, inheritFrom: "${inheritFrom}", serviceAccount: "helm",
-            containers: [
-				containerTemplate(name: 'helm', image: "${helmImage}", command: 'cat', ttyEnabled: true),
-                containerTemplate(name: 'gradle', image: "${gradleImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true),
-                containerTemplate(name: 'maven', image: "${mavenImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true, envVars: [ envVar(key: 'MAVEN_OPTS', value: '-Duser.home=/root/') ]),
-                containerTemplate(name: 'docker', image: "${dockerImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true)],
-            volumes: [configMapVolume(configMapName: 'jenkins-maven-settings', mountPath: '/root/.m2'),
-                      hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
-            envVars: [
-                envVar(key: 'DOCKER_HOST', value: 'unix:///var/run/docker.sock'),
-                envVar(key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/')]
+    podTemplate(label: label, inheritFrom: "${inheritFrom}", yaml: """
+    apiVersion: v1
+    kind: Pod
+    spec:
+      serviceAccount: helm
+      containers:
+      - name: docker
+        image: ${dockerImage}
+        command:
+        - cat
+        tty: true
+        securityContext:
+          privileged: true
+        env:
+        - name: DOCKER_HOST
+          value: unix:///var/run/docker.sock
+        volumeMounts:
+        - name: docker-socket
+          mountPath: /var/run/docker.sock
+      - name: clairscanner
+        image: ${clairScannerImage}
+        command:
+        - cat
+        tty: true
+        securityContext:
+          privileged: true
+        env:
+        - name: DOCKER_HOST
+          value: unix:///var/run/docker.sock
+        - name: POD_IP
+          valueFrom:
+            fieldRef:
+              fieldPath: status.podIP
+        volumeMounts:
+        - name: docker-socket
+          mountPath: /var/run/docker.sock
+        ports:
+        - containerPort: 9279
+          name: http
+          protocol: TCP
+      - name: helm
+        image: ${helmImage}
+        command:
+        - cat
+        tty: true
+      - name: gradle
+        image: ${gradleImage}
+        command:
+        - cat
+        tty: true
+        env:
+        - name: MAVEN_OPTS
+          value: -Duser.home=/root/
+        volumeMounts:
+        - name: maven-settings
+          mountPath: /root/.m2
+      volumes:
+      - name: docker-socket
+        hostPath:
+          path: /var/run/docker.sock
+          type: Socket
+      - name: maven-settings
+        configMap:
+          name: jenkins-maven-settings
+    """
     ) {
-        body()
-    }
+    body()
+  }
 }
