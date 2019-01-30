5cd7d309dde339318d97ea5c0834291851b160b3
Dale Tristram <dale.tristram@sprinthive.com>
2019-01-30 17:51:55 +0200

    Fix missing bracket

--- vars/standardPythonDeploy.groovy
+++ vars/standardPythonDeploy.groovy
@@ -50,7 +50,7 @@ def call(config) {
 
         if (env.POST_BUILD_TRIGGER_JOB != null) {
             stage("Trigger ${env.POST_BUILD_TRIGGER_JOB}") {
-                build job: env.POST_BUILD_TRIGGER_JOB, parameters: [string(name: 'IMAGE_TAG', value: versionTag), string(name: 'CHANGE_LOG', value: changeLog()], wait: false
+                build job: env.POST_BUILD_TRIGGER_JOB, parameters: [string(name: 'IMAGE_TAG', value: versionTag), string(name: 'CHANGE_LOG', value: changeLog())], wait: false
             }
         }
     }
