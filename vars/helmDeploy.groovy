edcc0db1d2621944790ef2fcca59e941221a2228
Dale Tristram <dale.tristram@sprinthive.com>
2021-03-14 11:01:21 +0200

    Fix helmfile command

--- vars/helmDeploy.groovy
+++ vars/helmDeploy.groovy
@@ -22,7 +22,7 @@ def call(config) {
     ])
 
     container('helm') {
-        def statusCode = sh script:"helmfile -f ${chartEnv}/helmfile.yaml --tiller-namespace ${config.namespace} --selector ${releaseName} --namespace ${config.namespace} sync --wait ${overrides}", returnStatus:true
+        def statusCode = sh script:"helmfile -f ${chartEnv}/helmfile.yaml --selector name=${releaseName} --namespace ${config.namespace} sync --wait ${overrides} --args \"--tiller-namespace ${config.namespace}\"", returnStatus:true
 
         if (statusCode != 0) {
             sh "helm --tiller-namespace ${config.namespace} rollback ${releaseName} ```helm --tiller-namespace ${config.namespace} history ${releaseName} | grep DEPLOYED | awk '{print \$1}' | tail -n 1```"
