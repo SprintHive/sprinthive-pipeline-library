05ecb90094f7c370b575903199a315ad2c7283d4
Dale Tristram <dale.tristram@sprinthive.com>
2022-10-11 17:17:13 +0200

    Move additionalOptions outside container

--- vars/grypeScan.groovy
+++ vars/grypeScan.groovy
@@ -1,11 +1,11 @@
 #!/usr/bin/groovy
 
 def call(imageName) {
+    def additionalOptions = ""
+    if (new File("grype.yaml").exists()) {
+        additionalOptions += "-c grype.yaml"
+    }
     container('grype-scanner') {
-        def additionalOptions = ""
-        if (new File("grype.yaml").exists()) {
-            additionalOptions += "-c grype.yaml"
-        }
         sh "/grype $imageName --fail-on high $additionalOptions"
     }
 }
