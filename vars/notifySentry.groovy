598b05e45e4126453cec387fe9861f13ee6c6673
Dale Tristram <dale.tristram@sprinthive.com>
2023-02-21 19:25:19 +0200

    Update sentry config

--- vars/notifySentry.groovy
+++ vars/notifySentry.groovy
@@ -2,20 +2,22 @@
 
 def call(config) {
     container('sentry') {
-        environment {
-            SENTRY_AUTH_TOKEN = credentials('sentry-auth-token')
-            SENTRY_ORG = 'sprinthive-7h'
-            SENTRY_PROJECT = 'sprinthive-console'
-            SENTRY_ENVIRONMENT = 'production'
-        }
+        def sentryOrg = "sprinthive-7h"
+        def sentryProject = "sprinthive-console"
+        def sentryEnvironment = "production"
+        def releaseName = env.BUILD_NUMBER
         steps {
-            sh '''
-                    export SENTRY_RELEASE=$BUILD_NUMBER
+            sh """
+                    export SENTRY_AUTH_TOKEN=${credentials('sentry-auth-token')}
+                    export SENTRY_ORG="${sentryOrg}"
+                    export SENTRY_PROJECT="${sentryProject}"
+                    export SENTRY_ENVIRONMENT="${sentryEnvironment}"
+                    export SENTRY_RELEASE="${releaseName}"
                     sentry-cli releases new -p $SENTRY_PROJECT $SENTRY_RELEASE
                     sentry-cli releases set-commits $SENTRY_RELEASE --auto
                     sentry-cli releases finalize $SENTRY_RELEASE
                     sentry-cli releases deploys $SENTRY_RELEASE new -e $SENTRY_ENVIRONMENT
-                '''
+               """
         }
     }
 }
