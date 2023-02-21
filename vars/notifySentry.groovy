ea4d279dd501fa8b129df0f061874577e960e087
Dale Tristram <dale.tristram@sprinthive.com>
2023-02-21 20:05:08 +0200

    Fix variables

--- vars/notifySentry.groovy
+++ vars/notifySentry.groovy
@@ -5,14 +5,15 @@ def call(config) {
     def sentryProject = "sprinthive-console"
     def sentryEnvironment = "production"
     def releaseName = env.BUILD_NUMBER
+    def authToken = credentials('sentry-auth-token')
     container('sentry') {
         sh """
-                export SENTRY_AUTH_TOKEN=${credentials('sentry-auth-token')}
+                export SENTRY_AUTH_TOKEN="${authToken}"
                 export SENTRY_ORG="${sentryOrg}"
-                sentry-cli releases new -p ${sentryProject} ${releaseName}
-                sentry-cli releases set-commits ${releaseName} --auto
-                sentry-cli releases finalize ${releaseName}
-                sentry-cli releases deploys ${releaseName} new -e ${sentryEnvironment}
+                sentry-cli releases new -p "${sentryProject}" "${releaseName}"
+                sentry-cli releases set-commits "${releaseName}" --auto
+                sentry-cli releases finalize "${releaseName}"
+                sentry-cli releases deploys "${releaseName}" new -e "${sentryEnvironment}"
            """
     }
 }
