cab2347147b8708a419e4e9ce0c4ab79993174e8
Dale Tristram <dale.tristram@sprinthive.com>
2023-02-21 20:11:14 +0200

    Fix credentials

--- vars/notifySentry.groovy
+++ vars/notifySentry.groovy
@@ -5,15 +5,15 @@ def call(config) {
     def sentryProject = "sprinthive-console"
     def sentryEnvironment = "production"
     def releaseName = env.BUILD_NUMBER
-    def authToken = credentials('sentry-auth-token')
     container('sentry') {
-        sh """
-                export SENTRY_AUTH_TOKEN="${authToken}"
+        withCredentials([string(credentialsId: "sentry-auth-token", variable: 'SENTRY_AUTH_TOKEN')]) {
+            sh """
                 export SENTRY_ORG="${sentryOrg}"
                 sentry-cli releases new -p "${sentryProject}" "${releaseName}"
                 sentry-cli releases set-commits "${releaseName}" --auto
                 sentry-cli releases finalize "${releaseName}"
                 sentry-cli releases deploys "${releaseName}" new -e "${sentryEnvironment}"
            """
+        }
     }
 }
