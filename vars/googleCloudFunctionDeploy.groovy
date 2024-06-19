0dc12966271fcb7e0cae198e24dbeca75e6b77c3
Anri Lombard <anrilombard@Anris-MacBook-Pro-2.local>
2024-06-19 14:37:54 +0200

    Create topic for pub sub

--- vars/googleCloudFunctionDeploy.groovy
+++ vars/googleCloudFunctionDeploy.groovy
@@ -1,74 +1,60 @@
-#!/usr/bin/groovy
-
-// This script currently supports deploying Google Cloud Functions with HTTP and Pub/Sub triggers.
-// TODO: figure out exact workload identity
-
-/**
- * Deploys a Google Cloud Function.
- *
- * @param config The configuration for deploying the Cloud Function.
- * @param config.functionName The name of the Cloud Function.
- * @param config.projectId The ID of the Google Cloud project where the function will be deployed.
- * @param config.serviceAccountEmail The email of the service account that will be used to run the function.
- * @param config.zipFilePath The path to the ZIP file containing the function's source code and dependencies.
- * @param config.region The region where the function will be deployed.
- * @param config.environmentVariables (Optional) A map of environment variables to be set for the function.
- * @param config.runtime The runtime environment for the function (e.g., 'python37', 'nodejs10').
- * @param config.entryPoint (Optional) The name of the function to be executed within the source code.
- * @param config.timeout (Optional) The timeout duration for the function execution.
- * @param config.maxInstances (Optional) The maximum number of instances for the function.
- * @param config.allowUnauthenticated (Optional) Whether to allow unauthenticated access to the function. Default is false.
- * @param config.triggerType The type of trigger for the function ('http' or 'pubsub').
- * @param config.topicName (Optional if not Pub/Sub triggered) The name of the Pub/Sub topic for Pub/Sub triggered functions.
- */
 def call(config) {
     def functionName = config.functionName
     def projectId = config.projectId
     def serviceAccountEmail = config.serviceAccountEmail
-    def zipFilePath = config.zipFilePath
+    def sourceCodeBucket = config.sourceCodeBucket
+    def sourceCodeObject = config.sourceCodeObject
     def region = config.region
     def environmentVariables = config.environmentVariables
     def runtime = config.runtime
     def entryPoint = config.entryPoint
     def timeout = config.timeout
     def maxInstances = config.maxInstances
-    def allowUnauthenticated = config.allowUnauthenticated ?: false
     def triggerType = config.triggerType
     def topicName = config.topicName
-
-    // Validation of required parameters
-    if (!functionName || !projectId || !serviceAccountEmail || !zipFilePath || !region || !triggerType || !runtime) {
-        error "Missing required parameters for deploying Cloud Function."
-    }
-
-    if (triggerType == 'pubsub' && !topicName) {
-        error "Missing required parameter 'topicName' for Pub/Sub triggered function."
-    }
+    def schedule = config.schedule
+    def timeZone = config.timeZone
+    def pubsubTargetData = config.pubsubTargetData
 
     ciNode {
         try {
             stage("Deploy Cloud Function: ${functionName}") {
                 container('gcloud') {
-                    withCredentials([
-                        googleServiceAccountKey(credentialsId: 'JENKINS_WORKER_GCP_SA_KEY')
-                    ]) {
+                    sh """
+                        gcloud config set project ${projectId}
+
+                        gcloud functions deploy ${functionName} \
+                            --runtime ${runtime} \
+                            --region ${region} \
+                            --source gs://${sourceCodeBucket}/${sourceCodeObject} \
+                            ${triggerType == 'http' ? '--trigger-http' : "--trigger-topic ${topicName}"} \
+                            --service-account ${serviceAccountEmail} \
+                            --set-env-vars ${environmentVariables.collect { "$it.key=$it.value" }.join(',')} \
+                            ${entryPoint ? "--entry-point ${entryPoint}" : ''} \
+                            ${timeout ? "--timeout ${timeout}" : ''} \
+                            ${maxInstances ? "--max-instances ${maxInstances}" : ''}
+                    """
+
+                    echo "Cloud Function ${functionName} deployed successfully."
+                }
+            }
+
+            if (triggerType == 'pubsub') {
+                stage("Create Pub/Sub Topic and Cloud Scheduler Job") {
+                    container('gcloud') {
                         sh """
                             gcloud config set project ${projectId}
 
-                            gcloud functions deploy ${functionName} \
-                                --runtime ${runtime} \
-                                --region ${region} \
-                                --source ${zipFilePath} \
-                                ${triggerType == 'http' ? '--trigger-http' : "--trigger-topic ${topicName}"} \
-                                --service-account ${serviceAccountEmail} \
-                                --set-env-vars ${environmentVariables.collect { "$it.key=$it.value" }.join(',')} \
-                                ${entryPoint ? "--entry-point ${entryPoint}" : ''} \
-                                ${timeout ? "--timeout ${timeout}" : ''} \
-                                ${maxInstances ? "--max-instances ${maxInstances}" : ''} \
-                                ${allowUnauthenticated ? '--allow-unauthenticated' : ''}
+                            gcloud pubsub topics create ${topicName}
+
+                            gcloud scheduler jobs create pubsub ${functionName}-scheduler \
+                                --schedule '${schedule}' \
+                                --topic ${topicName} \
+                                --message-body '${pubsubTargetData}' \
+                                --time-zone '${timeZone}'
                         """
 
-                        echo "Cloud Function ${functionName} deployed successfully."
+                        echo "Pub/Sub topic and Cloud Scheduler job created successfully."
                     }
                 }
             }
