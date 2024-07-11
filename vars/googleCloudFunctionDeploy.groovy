15a9f05ba45a806c02e76b41dc5b05deea0bd4b7
Anri Lombard <anri.m.lombard@sprinthive.com>
2024-07-11 14:43:21 +0200

    Remove check for update step

--- vars/googleCloudFunctionDeploy.groovy
+++ vars/googleCloudFunctionDeploy.groovy
@@ -46,86 +46,69 @@ def call(Map config) {
     ''') {
         node(podLabel) {
             container('gcloud') {
-
-                stage("Check for Changes and Prepare Function") {
-                    def hasChanges = false
+                stage("Prepare and Upload Function") {
                     dir(config.sourceFolderPath) {
-                        // Use git status to check for changes
-                        def status = sh(script: 'git status --porcelain', returnStdout: true).trim()
-                        hasChanges = !status.isEmpty()
-                        
-                        if (hasChanges) {
-                            echo "Changes detected in ${config.sourceFolderPath}"
-                            sh "tar -czf ${config.functionName}.tar.gz --exclude='.git' ."
-                            sh "mv ${config.functionName}.tar.gz .."
-                        } else {
-                            echo "No changes detected in ${config.sourceFolderPath}"
-                        }
+                        sh "tar -czf ${config.functionName}.tar.gz --exclude='.git' ."
+                        sh "mv ${config.functionName}.tar.gz .."
                     }
                     
-                    if (hasChanges) {
-                        stage("Upload Function") {
-                            sh "gcloud storage cp ${config.functionName}.tar.gz ${config.zipFilePath}"
-                            echo "Function tar.gz uploaded to ${config.zipFilePath}"
-                        }
+                    sh "gcloud storage cp ${config.functionName}.tar.gz ${config.zipFilePath}"
+                    echo "Function tar.gz uploaded to ${config.zipFilePath}"
+                }
 
-                        stage("Deploy Cloud Function: ${config.functionName}") {
-                            def deployCommand = """
-                                gcloud functions deploy ${config.functionName} \
-                                    --runtime ${config.runtime} \
-                                    --region ${config.region} \
-                                    --source ${config.zipFilePath} \
-                                    --service-account ${config.serviceAccountEmail} \
-                                    --set-env-vars ${config.environmentVariables.collect { "$it.key=$it.value" }.join(',')} \
-                                    ${config.entryPoint ? "--entry-point ${config.entryPoint}" : ''} \
-                                    ${config.timeout ? "--timeout ${config.timeout}" : ''} \
-                                    ${config.maxInstances ? "--max-instances ${config.maxInstances}" : ''} \
-                                    --verbosity debug
-                            """
+                stage("Deploy Cloud Function: ${config.functionName}") {
+                    def deployCommand = """
+                        gcloud functions deploy ${config.functionName} \
+                            --runtime ${config.runtime} \
+                            --region ${config.region} \
+                            --source ${config.zipFilePath} \
+                            --service-account ${config.serviceAccountEmail} \
+                            --set-env-vars ${config.environmentVariables.collect { "$it.key=$it.value" }.join(',')} \
+                            ${config.entryPoint ? "--entry-point ${config.entryPoint}" : ''} \
+                            ${config.timeout ? "--timeout ${config.timeout}" : ''} \
+                            ${config.maxInstances ? "--max-instances ${config.maxInstances}" : ''} \
+                            --verbosity debug
+                    """
 
-                            if (config.triggerType == 'http') {
-                                deployCommand += " --trigger-http"
-                            } else if (config.triggerType == 'pubsub') {
-                                deployCommand += " --trigger-topic ${config.topicName}"
-                            }
+                    if (config.triggerType == 'http') {
+                        deployCommand += " --trigger-http"
+                    } else if (config.triggerType == 'pubsub') {
+                        deployCommand += " --trigger-topic ${config.topicName}"
+                    }
 
-                            sh deployCommand
-                        }
+                    sh deployCommand
+                }
 
-                        if (config.triggerType == 'pubsub') {
-                            stage("Create Pub/Sub Topic and Cloud Scheduler Job") {
-                                sh """
-                                    gcloud scheduler jobs create pubsub ${config.functionName}-scheduler \
-                                        --schedule '${config.schedule}' \
-                                        --topic ${config.topicName} \
-                                        --message-body '${config.pubsubTargetData}' \
-                                        --time-zone '${config.timeZone}' || true
-                                """
-                            }
-                        }
+                if (config.triggerType == 'pubsub') {
+                    stage("Create Pub/Sub Topic and Cloud Scheduler Job") {
+                        sh """
+                            gcloud scheduler jobs create pubsub ${config.functionName}-scheduler \
+                                --schedule '${config.schedule}' \
+                                --topic ${config.topicName} \
+                                --message-body '${config.pubsubTargetData}' \
+                                --time-zone '${config.timeZone}' || true
+                        """
+                    }
+                }
 
-                        stage("Verify Cloud Function: ${config.functionName}") {
-                            sh """
-                                functionStatus=\$(gcloud functions describe ${config.functionName} --region ${config.region} --format='value(status)')
-                                if [ "\$functionStatus" != "ACTIVE" ]; then
-                                    echo "Cloud Function ${config.functionName} verification failed. Status: \$functionStatus"
-                                    exit 1
-                                fi
-                            """
+                stage("Verify Cloud Function: ${config.functionName}") {
+                    sh """
+                        functionStatus=\$(gcloud functions describe ${config.functionName} --region ${config.region} --format='value(status)')
+                        if [ "\$functionStatus" != "ACTIVE" ]; then
+                            echo "Cloud Function ${config.functionName} verification failed. Status: \$functionStatus"
+                            exit 1
+                        fi
+                    """
 
-                            if (config.triggerType == 'http') {
-                                sh """
-                                    functionUrl=\$(gcloud functions describe ${config.functionName} --region ${config.region} --format='value(httpsTrigger.url)')
-                                    response=\$(curl -s -o /dev/null -w '%{http_code}' \${functionUrl})
-                                    if [ "\$response" != "200" ]; then
-                                        echo "HTTP-triggered Cloud Function ${config.functionName} verification failed. HTTP response code: \$response"
-                                        exit 1
-                                    fi
-                                """
-                            }
-                        }
-                    } else {
-                        echo "No changes detected in ${config.sourceFolderPath}. Skipping deployment."
+                    if (config.triggerType == 'http') {
+                        sh """
+                            functionUrl=\$(gcloud functions describe ${config.functionName} --region ${config.region} --format='value(httpsTrigger.url)')
+                            response=\$(curl -s -o /dev/null -w '%{http_code}' \${functionUrl})
+                            if [ "\$response" != "200" ]; then
+                                echo "HTTP-triggered Cloud Function ${config.functionName} verification failed. HTTP response code: \$response"
+                                exit 1
+                            fi
+                        """
                     }
                 }
             }
