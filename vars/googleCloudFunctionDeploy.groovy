62a4c3e20abc4498ff32ac030515789b3fea917c
Anri Lombard <anri.m.lombard@sprinthive.com>
2024-07-19 13:39:27 +0200

    Attempt fix

--- vars/googleCloudFunctionDeploy.groovy
+++ vars/googleCloudFunctionDeploy.groovy
@@ -46,41 +46,6 @@ def call(Map config) {
     ''') {
         node(podLabel) {
             container('gcloud') {
-                // stage("Prepare and Upload Function") {
-                //     dir(config.sourceFolderPath) {
-                //         // Print current working directory and list its contents
-                //         sh "pwd && ls -la"
-                        
-                //         // Create the archive with verbose output
-                //         sh """
-                //             set -e
-                //             tar -cvzf ${config.functionName}.tar.gz --exclude='.git' .
-                            
-                //             # Verify the archive is not empty
-                //             if [ ! -s ${config.functionName}.tar.gz ]; then
-                //                 echo "Error: Created archive is empty"
-                //                 exit 1
-                //             fi
-                            
-                //             # List the contents of the archive
-                //             tar -tvf ${config.functionName}.tar.gz
-                //         """
-                        
-                //         // Move the archive to the parent directory
-                //         sh "mv ${config.functionName}.tar.gz .."
-                //     }
-                    
-                //     // Upload the archive to Google Cloud Storage
-                //     sh """
-                //         set -e
-                //         gcloud storage cp ${config.functionName}.tar.gz ${config.zipFilePath}
-                //         echo "Function tar.gz uploaded to ${config.zipFilePath}"
-                        
-                //         # Verify the uploaded file
-                //         gcloud storage ls -l ${config.zipFilePath}
-                //     """
-                // }
-
                 stage("Deploy Cloud Function: ${config.functionName}") {
                     def deployCommand = """
                         gcloud functions deploy ${config.functionName} \
