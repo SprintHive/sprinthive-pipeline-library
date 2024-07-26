32b0703d5c42a8e214ee1129430f8ab15a610d5a
Jacus Horne <jacus.horne@sprinthive.com>
2024-07-26 13:21:43 +0200

    Revert "Merge pull request #53 from SprintHive/integ-test-chart-override"
    This reverts commit 82854f5567522d761c4463ca31fb6601734fe4e8, reversing
    changes made to 77d72b54723ddfcd77afb47d29ca18338912c53f.

--- vars/republishDeployPipeline.groovy
+++ vars/republishDeployPipeline.groovy
@@ -1,10 +1,9 @@
 #!/usr/bin/groovy
-import groovy.json.JsonSlurperClassic
 
 /**
  * @param config.application: The application being deployed
  * @param config.integrationTest: (Optional) The integration test configuration (fields: enabled, repository, branch, envVars). If set and enabled, this will run the integration tests prior to deploying past test environments.
- * @param config.namespacesTest: (Optional, JSON String) if skipDeploy is true. The kubernetes test namespaces into which the application should be deployed without release approval.
+ * @param config.namespacesTest: (Optional) if skipDeploy is true. The kubernetes test namespaces into which the application should be deployed without release approval.
  * @param config.namespacesPreProd: (Optional) if skipDeploy is true. The kubernetes pre-prod namespaces into which the application should be deployed prior to full production rollout.
  * @param config.namespacesProd: (Optional) if skipDeploy is true. The kubernetes prod namespace into which the application should be deployed only with manual approval.
  * @param config.gcrCredentialsId: The credentials id for a GCR Service Account that can read from the source GCR \
@@ -26,23 +25,16 @@ def call(config) {
     println(params.changeLog)
   }
 
-  def namespacesTestParsed = parseJson(config.namespacesTest)
-  if (namespacesTestParsed != null && namespacesTestParsed.size() > 0) {
+  if (config.namespacesTest != null && config.namespacesTest.size() > 0) {
     cdNode {
-      namespacesTestParsed.each { item ->
-        stage("Helm Deploy: ${item.namespace}") {
-          def chartEnv = item.chartOverride
-          if (!chartEnv) {
-            chartEnv = item.namespace
-          }
-          withEnv(["CHART_ENVIRONMENT=${chartEnv}"]) {
-            helmDeploy([
-                    releaseName          : config.application,
-                    namespace            : item.namespace,
-                    imageTag             : params.imageTag,
-                    helmfileRepoOverride : config.helmfileRepoOverride
-            ])
-          }
+      for (namespaceTest in config.namespacesTest) {
+        stage("Helm Deploy: $namespaceTest") {
+          helmDeploy([
+                  releaseName          : config.application,
+                  namespace            : namespaceTest,
+                  imageTag             : params.imageTag,
+                  helmfileRepoOverride : config.helmfileRepoOverride
+          ])
         }
       }
     }
@@ -132,10 +124,4 @@ def call(config) {
       }
     }
   }
-}
-
-@NonCPS
-def parseJson(String jsonString) {
-  def jsonSlurper = new JsonSlurperClassic()
-  return jsonSlurper.parseText(jsonString)
 }
