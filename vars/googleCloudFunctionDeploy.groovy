c4807a19f72f9294ad83271722502d57b964fdf4
tshepo <tshepo.kau@sprinthive.com>
2024-08-12 17:19:50 +0200

    update google cloud function deploy

--- vars/googleCloudFunctionDeploy.groovy
+++ vars/googleCloudFunctionDeploy.groovy
@@ -9,27 +9,36 @@ import groovy.json.JsonSlurper
  * CLI docs: https://cloud.google.com/sdk/gcloud/reference/functions/deploy
  *
  * @param config A map containing configuration parameters for the function deployment:
- *   functionName        : Name of the Cloud Function (String, required)
- *   projectId           : Google Cloud Project ID (String, required)
- *                         - make sure the project has Jenkins worker as a principal (currently for qa and prod)
- *   gcsPath             : Google Cloud Storage path for the function archive (String, required)
- *   runtime             : Runtime for the Cloud Function (String, required)
- *   region              : Deployment region (String, required)
- *   serviceAccountEmail : Service account email for the function (String, required)
- *   environmentVariables: Map of environment variables for the function (Map, required)
- *   entryPoint          : Entry point for the function (String, optional)
- *   timeout             : Function timeout (String, optional)
- *   maxInstances        : Maximum number of function instances (Integer, optional)
- *   triggerType         : Type of trigger - 'http' or 'pubsub' (String, required)
- *   topicName           : PubSub topic name (required if triggerType is 'pubsub')
- *   generation          : Cloud Function generation - 'gen1' or 'gen2' (String, required)
- *   memory              : Memory allocation for the function (String, optional)
- *   concurrency         : Maximum number of concurrent requests (Integer, optional, gen2 only)
- *   cpu                 : CPU allocation for the function (Integer, optional, gen2 only)
+ *   functionName           : Name of the Cloud Function (String, required)
+ *   projectId              : Google Cloud Project ID (String, required)
+ *                              - make sure the project has Jenkins worker as a principal (currently for qa and prod)
+ *   runtime                : Runtime for the Cloud Function (String, required)
+ *   region                 : Deployment region (String, required)
+ *   serviceAccountEmail    : Service account email for the function (String, optional)
+ *   environmentVariables   : Map of environment variables for the function (Map, optional)
+ *   entryPoint             : Entry point for the function (String, optional)
+ *   timeout                : Function timeout (String, optional)
+ *   maxInstances           : Maximum number of function instances (Integer, optional)
+ *   triggerType            : Type of trigger - 'http' or 'pubsub' (String, required)
+ *   topicName              : PubSub topic name (required if triggerType is 'pubsub')
+ *   generation             : Cloud Function generation - 'gen1' or 'gen2' (String, required)
+ *   memory                 : Memory allocation for the function (String, optional)
+ *   concurrency            : Maximum number of concurrent requests (Integer, optional, gen2 only)
+ *   cpu                    : CPU allocation for the function (Integer, optional, gen2 only)
+ *   ingress                : Ingress settings controlling what traffic can reach the function ('all' || 'internal-only' || 'internal-and-gclb', optional, default 'all')
+ *   allowUnauthenticated   : If true this will allow all callers, without checking authentication (boolean, optional, default false)
  */
 def call(Map config) {
     // Validate required parameters
-    def requiredParams = ['functionName', 'projectId', 'gcsPath', 'runtime', 'region', 'serviceAccountEmail', 'environmentVariables', 'triggerType', 'generation']
+    def requiredParams = [
+            'functionName',
+            'projectId',
+            'runtime',
+            'region',
+            'environmentVariables',
+            'triggerType',
+            'generation'
+    ]
     requiredParams.each { param ->
         if (!config.containsKey(param)) {
             error "Missing required parameter: ${param}"
@@ -44,45 +53,21 @@ def call(Map config) {
         error "Invalid generation specified. Must be 'gen1' or 'gen2'."
     }
 
-    stage('Prepare Function Archive') {
-        prepareArchive(config.functionName)
+    if (!['all', 'internal-only', 'internal-and-gclb', null].contains(config.ingress)) {
+        error "Invalid ingress specified. Must be 'all', 'internal-only', 'internal-and-gclb'."
     }
-    
+
     def podLabel = "gcloud-${UUID.randomUUID().toString()}"
-    
+
     podTemplate(label: podLabel, yaml: getGcloudPodYaml()) {
         node(podLabel) {
-            stage('Copy Function Archive') {
-                copyArchive(config.functionName)
-            }
-
             container('gcloud') {
-                stage("Upload Function Archive") {
-                    uploadArchive(config)
-                }
-
                 stage("Deploy Cloud Function: ${config.functionName}") {
                     deployFunction(config)
                 }
             }
         }
     }
-}
-
-def prepareArchive(String functionName) {
-    def archiveName = "${functionName}.zip"
-    
-    sh """
-        cd ${env.WORKSPACE}/${functionName}
-        jar -cvf ../${archiveName} .
-        cd ..
-        if [ ! -s ${archiveName} ]; then
-            echo "Error: Created archive is empty"
-            exit 1
-        fi
-    """
-    
-    archiveArtifacts artifacts: archiveName, fingerprint: true
 }
 
 def getGcloudPodYaml() {
@@ -103,41 +88,9 @@ def getGcloudPodYaml() {
     """
 }
 
-def copyArchive(String functionName) {
-    container('gcloud') {
-        script {
-            try {
-                def archiveName = "${functionName}.zip"
-                copyArtifacts(
-                    projectName: env.JOB_NAME, 
-                    selector: specific(env.BUILD_NUMBER), 
-                    filter: archiveName, 
-                    fingerprintArtifacts: true
-                )
-                
-                sh """
-                    if [ ! -f "${archiveName}" ]; then
-                        echo "Error: Archive file not found in current directory"
-                        exit 1
-                    fi
-                """
-            } catch (Exception e) {
-                error "Failed to process function archive: ${e.message}"
-            }
-        }
-    }
-}
-
-def uploadArchive(Map config) {
-    sh """
-        gcloud config set project ${config.projectId}
-        gcloud storage cp ${config.functionName}.zip ${config.gcsPath}
-    """
-}
-
 def deployFunction(Map config) {
     def deployCommand = "gcloud functions deploy ${config.functionName}"
-    
+
     if (config.generation == 'gen2') {
         // Jenkins is very sensitive to indentation, so this snippet looks strange but it's correct
         deployCommand += """ --gen2 \\
@@ -147,20 +100,20 @@ def deployFunction(Map config) {
         // Have to explicitly set since gen2 is becoming the default
         deployCommand += " --no-gen2 \\"
     }
-    
+
     deployCommand += """
+        --project ${config.projectId} \\
         --runtime ${config.runtime} \\
         --region ${config.region} \\
-        --source ${config.gcsPath} \\
-        --service-account ${config.serviceAccountEmail} \\
-        --set-env-vars ${config.environmentVariables.collect { "${it.key}=${it.value}" }.join(',')} \\
+        ${config.ServiceAccountEmail != null ? "--service-account " + config.serviceAccountEmail : ""}  \\
+        ${config.environmentVariables != null && !config.environmentVariables.isEmpty() ? "--set-env-vars " + config.environmentVariables.collect { "${it.key}=${it.value}" }.join(',') : "" } \\
+        --ingress-settings ${config.ingress != null ? config.ingress : 'all'} \\
         ${config.entryPoint ? "--entry-point ${config.entryPoint}" : ''} \\
         ${config.timeout ? "--timeout ${config.timeout}" : ''} \\
         ${config.maxInstances ? "--max-instances ${config.maxInstances}" : ''} \\
         ${config.memory ? "--memory=${config.memory}" : ''} \\
         ${config.allowUnauthenticated ? "--allow-unauthenticated" : '--no-allow-unauthenticated'} \\
     """
-
     if (config.triggerType == 'http') {
         deployCommand += "    --trigger-http"
     } else if (config.triggerType == 'pubsub') {
