79435bb51bc6d75e8082f97b0acfc52c1a1bf30f
Kieran Roberts <kieran.roberts@sprinthive.com>
2024-08-30 14:06:41 +0200

    Test upgrading java

--- vars/ciNode.groovy
+++ vars/ciNode.groovy
@@ -2,7 +2,7 @@
 def call(Map parameters = [:], body) {
     def label = parameters.get('label', buildId('ciNode'))
 
-    def gradleImage = parameters.get('gradleImage', 'gradle:5.1-jdk-alpine')
+    def gradleImage = parameters.get('gradleImage', 'gradle:7.5.1-jdk-alpine')
     def grypeScannerImage = parameters.get('grypeScannerImage', 'anchore/grype:debug')
     def kanikoImage = parameters.get('kanikoImage', 'gcr.io/kaniko-project/executor:debug')
     def craneImage = parameters.get('craneImage', 'gcr.io/go-containerregistry/gcrane:debug')
