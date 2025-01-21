0b076ffa02806adabca4affb992e8732a3fec997
Kieran Roberts <kieran.roberts@sprinthive.com>
2025-01-21 13:35:47 +0200

    Remove unnecessary toleration

--- vars/ciNode.groovy
+++ vars/ciNode.groovy
@@ -17,10 +17,6 @@ def call(Map parameters = [:], body) {
           operator: Equal
           value: "arm64"
           effect: NoSchedule
-        - key: sprinthive.com/purpose
-          operator: Equal
-          value: "sh-services"
-          effect: NoSchedule    
     """
     def armNodeSelector = """
         sprinthive.com/instance-type: "c4a"
