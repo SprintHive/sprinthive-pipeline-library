83a858a81f1d110681ecd5d11bdbcc48f4c27c2c
Kieran Roberts <kieran.roberts@sprinthive.com>
2025-01-22 14:17:36 +0200

    bump tf version

--- vars/ciNode.groovy
+++ vars/ciNode.groovy
@@ -7,7 +7,7 @@ def call(Map parameters = [:], body) {
     def kanikoImage = parameters.get('kanikoImage', 'gcr.io/kaniko-project/executor:debug')
     def craneImage = parameters.get('craneImage', 'gcr.io/go-containerregistry/gcrane:debug')
     def helmImage = parameters.get('helmImage', 'quay.io/roboll/helmfile:v0.144.0')
-    def terraformImage = parameters.get('terraformImage', 'hashicorp/terraform:1.6.6')
+    def terraformImage = parameters.get('terraformImage', 'hashicorp/terraform:1.9.8')
     def nodejsImage = parameters.get('nodejsImage', 'node:20-alpine')
     def inheritFrom = parameters.get('inheritFrom', 'default')
 
