74077c91bd24242d874e969309fa454a11cd907f
Kieran Roberts <kieran.roberts@sprinthive.com>
2025-01-27 13:06:39 +0200

    test

---
+++ vars/terraformPod.groovy
@@ -0,0 +1,67 @@
+#!/usr/bin/groovy
+def call(Map parameters = [:], body) {
+    def label = parameters.get('label', buildId('ciNode'))
+    def arch = parameters.get('arch', 'amd64')
+    def buildArm = arch == 'arm64' ? true : false
+    def terraformImage = parameters.get('terraformImage', 'europe-west1-docker.pkg.dev/sh-qa-00/dev-containers/terraform-deploy:0.4')
+
+
+
+    def armTolerations = """
+        - key: kubernetes.io/arch
+          operator: Equal
+          value: "arm64"
+          effect: NoSchedule
+    """
+    def armNodeSelector = """
+        sprinthive.com/instance-type: "c4a"
+    """
+    echo "Starting CI node"
+    echo "Building for: ${arch}"
+
+    podTemplate(label: label, inheritFrom: "${inheritFrom}", yaml: """
+    apiVersion: v1
+    kind: Pod
+    ServiceAccount: Terraform
+    spec:
+      tolerations:
+        ${buildArm ? armTolerations : ''}
+      nodeSelector:
+        ${buildArm ? armNodeSelector : ''}
+      containers:
+      - name: terraform
+        image: ${terraformImage}
+        
+        env:
+        - name: AWS_ACCESS_KEY_ID
+          valueFrom:
+            secretKeyRef:
+              name: terraform-dev-aws-credentials
+              key: accessKey
+        - name: AWS_SECRET_ACCESS_KEY
+          valueFrom:
+            secretKeyRef:
+              name: terraform-dev-aws-credentials
+              key: secretKey
+        volumeMounts:
+        - name: ssh-config
+          mountPath: "/dump/"
+        tty: true
+        resources:
+          requests:
+            memory: 128Mi
+      volumes:
+      - name: maven-settings
+        configMap:
+          name: jenkins-maven-settings
+          optional: true
+      - name: ssh-config
+        secret:
+          secretName: jenkins-ssh-config
+    """
+    ) {
+        node(label) {
+            body()
+        }
+    }
+}
