8261a1df3089d0d8c0fa1f1331f6b3435e0f625d
Kieran Roberts <kieran.roberts@sprinthive.com>
2025-06-06 21:26:22 +0200

    Test dynamic workspaces

--- vars/terraformPlan.groovy
+++ vars/terraformPlan.groovy
@@ -7,7 +7,7 @@ def call(config) {
             branches: [[name: 'dev']],
             userRemoteConfigs: [[credentialsId: 'bitbucket', url: params.GIT_URL]]
     ])
-
+        
         def targetArguments = []
         if (config.TF_TARGETTED_RESOURCES) {
             config.TF_TARGETTED_RESOURCES.eachLine { line ->
@@ -19,15 +19,18 @@ def call(config) {
         }
 
         container('terraform') {
-            stage('Terraform Plan') {
-                sh script: 'mkdir ~/.ssh/ && cp /dump/id_rsa ~/.ssh/id_rsa && chmod 0600 ~/.ssh/id_rsa && cp /dump/known_hosts ~/.ssh/known_hosts && cp /dump/config ~/.ssh/config'
-                sh script: "cd ${config.TF_DIRECTORY} && terraform init && vault login -no-print --method gcp role=terraform-dev  && terraform workspace select ${params.TF_WORKSPACE_EXPLICIT} && terraform plan -out plan.tfplan ${targetArguments.join(' ')}"
-            //archiveArtifacts artifacts: 'plan.tfplan', fingerprint: true
+            params.targetWorkspaces.each { workspace ->
+                stage("Terraform Plan: ${workspace}") {
+                  sh script: 'mkdir ~/.ssh/ && cp /dump/id_rsa ~/.ssh/id_rsa && chmod 0600 ~/.ssh/id_rsa && cp /dump/known_hosts ~/.ssh/known_hosts && cp /dump/config ~/.ssh/config'
+                  sh script: "cd ${config.TF_DIRECTORY} && terraform init && vault login -no-print --method gcp role=terraform-dev  && terraform workspace select ${workspace} && terraform plan -out ${workspace}-plan.tfplan ${targetArguments.join(' ')}"
+                  archiveArtifacts artifacts: '${workspace}-plan.tfplan', fingerprint: true
+                }
+                stage("Terraform Apply: ${workspace}") {
+                  input message: 'Review the Terraform plan. Proceed with apply?', ok: 'Apply', cancel: 'Abort'
+                  sh script: "cd ${config.TF_DIRECTORY} && terraform workspace select ${config.TF_WORKSPACE_EXPLICIT} && terraform apply ${workspace}-plan.tfplan"
+                }
             }
-            stage('Terraform Apply') {
-                input message: 'Review the Terraform plan. Proceed with apply?', ok: 'Apply', cancel: 'Abort'
-                sh script: "cd ${config.TF_DIRECTORY} && terraform workspace select ${config.TF_WORKSPACE_EXPLICIT} && terraform apply plan.tfplan"
-             }
+
         }
     }
 }
