6001a25fa7be87039a137fce0637b08d1ec8ee07
Kieran Roberts <kieran.roberts@sprinthive.com>
2025-06-06 23:39:45 +0200

    Return scripts SH

--- vars/terraformPlan.groovy
+++ vars/terraformPlan.groovy
@@ -23,12 +23,13 @@ def call(config) {
         }
 
         container('terraform') {
-            sh script: "mkdir -p ~/.ssh/ ${config.TF_DIRECTORY}/logs/plans ${config.TF_DIRECTORY}/plans && chmod -R 777 ${config.TF_DIRECTORY}/logs/plans  && cp /dump/id_rsa ~/.ssh/id_rsa && chmod 0600 ~/.ssh/id_rsa && cp /dump/known_hosts ~/.ssh/known_hosts && cp /dump/config ~/.ssh/config"
+            sh script: "vault login -no-print --method gcp role=terraform-dev  && mkdir -p ~/.ssh/ ${config.TF_DIRECTORY}/logs/plans ${config.TF_DIRECTORY}/plans && chmod -R 777 ${config.TF_DIRECTORY}/logs/plans  && cp /dump/id_rsa ~/.ssh/id_rsa && chmod 0600 ~/.ssh/id_rsa && cp /dump/known_hosts ~/.ssh/known_hosts && cp /dump/config ~/.ssh/config"
             for (workspace in targetWorkspaces) {
                 stage("Terraform Plan: ${workspace}") {
                     
-                    script: "vault login -no-print --method gcp role=terraform-dev  &&  cd ${config.TF_DIRECTORY} && terraform init && terraform workspace select ${workspace} && terraform plan -out plans/${workspace}.tfplan ${targetArguments.join(' ')}"
+                    sh script: "cd ${config.TF_DIRECTORY} && terraform init && terraform workspace select ${workspace} && terraform plan -out plans/${workspace}.tfplan ${targetArguments.join(' ')} | tee -a logs/plans/${workspace}.log"
 
+                    archiveArtifacts artifacts: "${config.TF_DIRECTORY}/logs/plans/${workspace}.log", fingerprint: true
                     archiveArtifacts artifacts: "${config.TF_DIRECTORY}/plans/${workspace}.tfplan", fingerprint: true
                 }
                 stage("Terraform Apply: ${workspace}") {
