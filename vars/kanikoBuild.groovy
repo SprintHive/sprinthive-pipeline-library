fc28e891b6a51f4767e2fc9b5a42cacffccc20e2
Anri Lombard <anri.m.lombard@sprinthive.com>
2025-07-14 14:50:16 +0200

    compress caching kaniko

--- vars/kanikoBuild.groovy
+++ vars/kanikoBuild.groovy
@@ -1,7 +1,24 @@
 #!/usr/bin/groovy
 
 def call(contextDir, destinationTarPath, destination, sourceVersion) {
+    def extraArgs = [
+        '--snapshot-mode=redo',
+        '--cache=true',
+        '--cache-repo=eu.gcr.io/sh-honeycomb/kaniko-cache',
+        '--compressed-caching'
+    ].join(' ')
+
     container('kaniko') {
-        sh "/kaniko/executor --dockerfile \"$contextDir/Dockerfile\" --context \"$contextDir\" --tar-path \"$destinationTarPath\" --destination \"$destination\" --build-arg SOURCE_VERSION=\"$sourceVersion\" --log-format text --no-push"
+        sh """
+          /kaniko/executor \
+            --dockerfile "${contextDir}/Dockerfile" \
+            --context "${contextDir}" \
+            --tar-path "${destinationTarPath}" \
+            --destination "${destination}" \
+            --build-arg SOURCE_VERSION="${sourceVersion}" \
+            ${extraArgs} \
+            --log-format text \
+            --no-push
+        """
     }
 }
