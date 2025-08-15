9db496e5a536dd00d66f5236fc0262f43dbebe56
Kieran Roberts <kieran.roberts@sprinthive.com>
2025-08-15 11:36:46 +0200

    Fix \

--- vars/terraformPlan.groovy
+++ vars/terraformPlan.groovy
@@ -24,19 +24,19 @@ void call(Map config) {
 
         container('terraform') {
             sh script: """
-                vault login -no-print --method gcp role=terraform-dev  && \
-                mkdir -p ~/.ssh/ ${config.TF_DIRECTORY}/logs/plans ${config.TF_DIRECTORY}/plans && \
+                vault login -no-print --method gcp role=terraform-dev  && \\
+                mkdir -p ~/.ssh/ ${config.TF_DIRECTORY}/logs/plans ${config.TF_DIRECTORY}/plans && \\
                 # Required for Git Auth
-                cp /dump/id_rsa ~/.ssh/id_rsa && chmod 0600 ~/.ssh/id_rsa && \
-                cp /dump/known_hosts ~/.ssh/known_hosts && \
+                cp /dump/id_rsa ~/.ssh/id_rsa && chmod 0600 ~/.ssh/id_rsa && \\
+                cp /dump/known_hosts ~/.ssh/known_hosts && \\
                 cp /dump/config ~/.ssh/config
             """
             sh script: 'terraform init'
             for (workspace in targetWorkspaces) {
                 stage("Terraform Plan: ${workspace}") {
                     sh script: """
-                      cd ${config.TF_DIRECTORY} && terraform workspace select ${workspace} && \
-                      terraform plan -out plans/${workspace}.tfplan ${targetArguments.join(' ')} && \
+                      cd ${config.TF_DIRECTORY} && terraform workspace select ${workspace} && \\
+                      terraform plan -out plans/${workspace}.tfplan ${targetArguments.join(' ')} \\
                       | tee -a logs/plans/${workspace}.log
                     """
                     archiveArtifacts artifacts: "${config.TF_DIRECTORY}/logs/plans/${workspace}.log", fingerprint: true
@@ -45,9 +45,9 @@ void call(Map config) {
                 stage("Terraform Apply: ${workspace}") {
                     input message: 'Review the Terraform plan. Proceed with apply?', ok: 'Apply', cancel: 'Abort'
                     sh script: """
-                      cd ${config.TF_DIRECTORY} && \
-                      terraform workspace select ${workspace} && \
-                      terraform apply plans/${workspace}.tfplan \
+                      cd ${config.TF_DIRECTORY} && \\
+                      terraform workspace select ${workspace} && \\
+                      terraform apply plans/${workspace}.tfplan \\
                       | tee -a logs/plans/${workspace}.log
                     """
                 }
